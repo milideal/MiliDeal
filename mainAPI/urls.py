2e5708be3d8c21007dac3a7b753bc23fa070c55b
woohyun <woohyun212@gmail.com>
2023-09-14 14:54:46 +0000

    Change ViewSet - Change SimpleRouter to DefaultRouter

--- mainAPI/urls.py
+++ mainAPI/urls.py
@@ -1,10 +1,9 @@
 from rest_framework import routers
 from .views import TestViewSets
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register('test', TestViewSets)
 
-urlpatterns = [
-    # path('some_functional_view_rul', views.functional_view)
-]
-urlpatterns += router.urls
+urlpatterns = router.urls
+    # [path('some_functional_view_rul', views.functional_view)]
+
