914425b3d4028bc9027d4c10e8cbed4e2aaf6d92
woohyun <woohyun212@gmail.com>
2023-09-15 16:43:00 +0000

    Create KAKAO REST API code to get coordinate info

--- Util/kakao_geo.py
+++ Util/kakao_geo.py
@@ -9,11 +9,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 HOST = "https://dapi.kakao.com"
 URL = "/v2/local/search/address"
 API_KEY = env_keys('KAKAO_RESTAPI_KEY')
-headers={"Authorization":f"KakaoAK {API_KEY}"}
-params = {"query":"제주도 서귀포시 상예로 319", 
-"analyze_type": "exact"}
+headers = {"Authorization": f"KakaoAK {API_KEY}"}
+params = {"query": "제주도 서귀포시 상예로 319",
+          "analyze_type": "exact"}
 
-r = requests.get(HOST+URL, headers=headers, params=params) 
+r = requests.get(HOST+URL, headers=headers, params=params)
 
 print(r.url)
 print(r.status_code)
