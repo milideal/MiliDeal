d94dc7a2ea5e60259c9e3f2a8c849e2a8053956a
woohyun <woohyun212@gmail.com>
2023-09-16 17:13:45 +0000

    feat(store.coord): Change response form

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import StoreModel
 
-
 class StoreSerializer(serializers.HyperlinkedModelSerializer):
+    coord = serializers.SerializerMethodField()
+    coordx = serializers.FloatField(write_only = True)
+    coordy = serializers.FloatField(write_only = True)
     class Meta:
         model = StoreModel
-        fields = '__all__'
+        # fields = ["url","slug", "address", "name", "coord", "coordx", "coordy",
+        # "storeType", "imageSrc", "target", "promotion", "tel",  "facilities", "homepage", "endDate"]
+        fields = "__all__"
+        extra_kwargs = {
+            'url': {'lookup_field': 'slug'}
+        }
+        
+    
+    def get_coord(self, obj):
+        return {"x": obj.coordx, "y": obj.coordy}
