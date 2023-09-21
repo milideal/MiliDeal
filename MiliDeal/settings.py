139f1223d915cfba5c5abf233f188a2c72d7c449
callendev20 <callendev20@gmail.com>
2023-09-21 17:10:02 +0900

    Add duplicate detector

--- MiliDeal/settings.py
+++ MiliDeal/settings.py
@@ -82,6 +82,7 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None  # username 필드 사용 x
 ACCOUNT_EMAIL_REQUIRED = True             # email 필드 사용 o
 ACCOUNT_USERNAME_REQUIRED = False         # username 필드 사용 x
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'       # 회원가입 과정에서 이메일 인증 사용 X
 
 # Database
--- user/models.py
+++ user/models.py
@@ -1,21 +1,62 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from rest_framework import status
+from rest_framework.response import Response
+from pymongo import MongoClient
+
+
+def isDuplicate(user_email: str) -> bool:
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['test']
+    collection = db['account_emailaddress']
+    existing_user = collection.find_one({'email': user_email})
+
+    # 이미 같을 이메일을 사용하는 유저가 있을 경우 중복 처리
+    if existing_user:
+        print("중복된 이메일입니다.")
+        return True
+    else:
+        return False
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def create_user(self, email, password, **kwargs):
+        print("================= Create User =================")
         if not email:
             raise ValueError('Users must have an email address')
+
+        if isDuplicate(user_email=email):
+            print(f"중복된 키 오류")
+            return Response({"error": "이미 존재하는 데이터입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
         user = self.model(
             email=self.normalize_email(email),
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
+        # try:
+        #     user.save(using=self._db)
+        #     return user
+        # except BulkWriteError as bwe:
+        #     for error in bwe.details['writeErrors']:
+        #         if error['code'] == 11000:  # 중복 키 오류 코드 (E11000)
+        #             duplicate_key = error['keyValue']
+        #             print(f"중복된 키 오류: {duplicate_key}")
+        #             return Response({"error": "이미 존재하는 데이터입니다."}, status=status.HTTP_400_BAD_REQUEST)
+        # except Exception as e:
+        #     # 다른 예외 처리
+        #     print(f"다른 예외 발생: {str(e)}")
+        #     return Response({"error": "데이터 처리 중 오류가 발생했습니다."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def create_superuser(self, email=None, password=None, **extra_fields):
+
+        if isDuplicate(user_email=email):
+            print(f"중복된 키 오류")
+            return Response({"error": "이미 존재하는 데이터입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
         superuser = self.create_user(
             email=email,
             password=password,
