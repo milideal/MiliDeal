b99723fb63d39d8e76f488817b2420cfbf89c258
callendev20 <callendev20@gmail.com>
2023-09-22 12:33:21 +0900

    🛠️  Add nickname field to user model

--- user/models.py
+++ user/models.py
@@ -16,16 +16,17 @@ class UserManager(DjongoManager):
         else:
             return False
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, nickname=None, **extra_fields):
         if not email:
             raise ValueError(_('The Email field must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+
+        user = self.model(email=email, nickname=nickname, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email=None, password=None, **extra_fields):
+    def create_superuser(self, email=None, password=None, nickname=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -34,7 +35,7 @@ class UserManager(DjongoManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, nickname, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -42,6 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # username = models.TextField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=30, blank=True)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
