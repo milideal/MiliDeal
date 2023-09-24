fa4538c76dfdb949cd325f15d66371bcebaf24eb
yeonjun <yeonjunky@gmail.com>
2023-09-24 07:56:22 +0000

    register review app

--- MiliDeal/urls.py
+++ MiliDeal/urls.py
@@ -7,11 +7,15 @@ from rest_framework import routers
 from mainAPI.urls import router as mainAPI_router
 from store.urls import router as store_router
 from user.urls import router as user_router
+from review.urls import router as review_router
+
 
 root_router = routers.DefaultRouter(trailing_slash=False)
 root_router.registry.extend(mainAPI_router.registry)
 root_router.registry.extend(store_router.registry)
 root_router.registry.extend(user_router.registry)
+root_router.registry.extend(review_router.registry)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
