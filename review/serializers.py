0bc9404261bf542f26ff933d843f8f2857dda1e9
yeonjun <yeonjunky@gmail.com>
2023-09-24 11:28:15 +0000

    exclude _id field

--- review/serializers.py
+++ review/serializers.py
@@ -6,6 +6,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Review
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('_id', )
 
     
