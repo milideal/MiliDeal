4e0ce7f3f7cdbab4bedc2f83b62260ca374a6c61
woohyun <woohyun212@gmail.com>
2023-09-26 16:33:42 +0000

    ⚡️Update query to receive only slug data from DB - find 가 아니라 aggregate를 이용하려다 보니까 pipeline 에 대해 공부할 필요가 있었네요. - 최적화 하는 의미가 있을지?

--- Util/mongo_geo.py
+++ Util/mongo_geo.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     import os, sys
-    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
     from pprint import pprint
 
 from MiliDeal.settings import env_keys 
@@ -14,13 +14,26 @@ mongo_client = MongoClient(
     password=env_keys('mongo_password')
 )
 
-# 위치 좌표 기준 N meter 이내(원) 정보 가져옴
-def get_stores_geoWithin_search(coordinates:list, meter:int):
-    return list(mongo_client[env_keys('mongo_database')].stores.find({
-        "coord": {
-            "$geoWithin": {
-            "$centerSphere": [coordinates, meter / 6378160]
-                }
-            }
-        })
-    )
+# 위치 좌표 기준 N meter 이내(원) slugs를 가져옴
+# def get_slugs_with_geoWithin_search(coordinates:tuple, distance:int):
+#     return list(mongo_client[env_keys('mongo_database')].stores.find({
+#         "coord": {
+#             "$geoWithin": {
+#             "$centerSphere": [coordinates, distance / 6378160]
+#                 }
+#             }
+#         }, {"_id": 0, "slug":1 })
+#     )
+
+
+def get_slugs_with_geoWithin_search(coordinates:list, distance:int):
+    pipeline = []
+    pipeline.append({"$match": {"coord": {
+        "$geoWithin":{"$centerSphere": [coordinates, 96 / 6378160]}}}})
+    pipeline.append({"$group": {"_id" : "slugs","slugs": { "$push": "$slug" }}})
+    pipeline.append({"$project": { "_id":0} })
+    return list(mongo_client[env_keys('mongo_database')].stores.aggregate(pipeline))[0]["slugs"]
+
+if __name__ == "__main__":
+    searched_slugs = get_slugs([127.7816977, 37.9296045], 120)
+    pprint(searched_slugs)
