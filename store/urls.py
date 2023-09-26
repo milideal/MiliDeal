55ea7df765f65ab3b2cb2f112b8f413f4ed90342
woohyun <woohyun212@gmail.com>
2023-09-26 13:24:15 +0000

    🔧Add `StoreGeoSearchViewSets`'s url

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import StoreViewSets
+from .views import StoreViewSets, StoreGeoSearchViewSets
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'store', StoreViewSets)
+router.register(r'geo', StoreGeoSearchViewSets, "geo")
 
 urlpatterns = []
