19c8e3464a1d9c9c22e9401d8abd83a08ef6ddd7
woohyun <woohyun212@gmail.com>
2023-09-28 13:45:59 +0000

    🔧Add STATIC_ROOT for deployment

--- MiliDeal/settings.py
+++ MiliDeal/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + STATIC_URL
 
 # Django REST Framework
 REST_FRAMEWORK = {
