60bea5f64ed2f63d8d2a6b28b5f352f40e0e4d7d
yeonjun <yeonjunky@gmail.com>
2023-09-29 05:49:27 +0000

    inherit update, destroy for apply convert_objectId decorator

--- review/views.py
+++ review/views.py
@@ -1,13 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
-
+from rest_framework.authtoken.models import Token
 
 from review.serializers import ReviewSerializer
 from review.models import Review
 from review.ReviewPagination import ReviewPagination
 from review.IsAuthorOrReadOnly import IsAuthorOrReadOnly
 from store.models import StoreModel
+from Util.decorators import convert_objectId
 
 # Create your views here.
 class ReviewViewSet(ModelViewSet):
@@ -15,12 +16,19 @@ class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     pagination_class = ReviewPagination
     permission_classes = [IsAuthenticatedOrReadOnly]
-    lookup_field = 'slug'
 
     def perform_create(self, serializer):
         store_slug = self.request.query_params.get('slug')
         store = get_object_or_404(StoreModel, slug=store_slug)
 
-        author = self.request.user()
+        author = self.request._user
 
         serializer.save(review_of=store, author=author)
+
+    @convert_objectId
+    def update(self, request, *args, **kwargs):
+        return super().update(request, args, kwargs)
+
+    @convert_objectId
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, args, kwargs)
