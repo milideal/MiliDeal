9193d47d810d21f0b640632461f70e20fc214cdb
yeonjun <yeonjunky@gmail.com>
2023-10-01 06:35:32 +0000

    :sparkles: change HTTP method for func create

--- review/urls.py
+++ review/urls.py
@@ -5,6 +5,16 @@ from review.views import ReviewViewSet
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'review', ReviewViewSet)
 
+router.routes[0] = routers.Route(
+        url=r'^{prefix}{trailing_slash}$',
+        mapping={
+            'get': 'list',
+            'put': 'create'
+        },
+        name='{basename}-list',
+        detail=False,
+        initkwargs={'suffix': 'List'}
+    )
 
 urlpatterns = []
 # [path('some_functional_view_rul', views.functional_view)]
