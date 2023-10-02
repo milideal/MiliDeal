74a5f50a47b8c7856b0d10b85efcc90bda5c4980
woohyun <woohyun212@gmail.com>
2023-10-02 15:59:06 +0000

    🗃️ State DB Table name of review

--- review/models.py
+++ review/models.py
@@ -1,24 +1,26 @@
 from djongo import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
 from store.models import StoreModel
 from user.models import User
 
-# Create your models here.
+
 class Review(models.Model):
     _id = models.ObjectIdField()
 
     review_of = models.ForeignKey(
-        StoreModel, 
-        on_delete=models.CASCADE, 
+        StoreModel,
+        on_delete=models.CASCADE,
         related_name="reviews"
     )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     review = models.TextField(null=False)
     score = models.FloatField(
-        null=False, 
+        null=False,
         validators=[MinValueValidator(0), MaxValueValidator(5)]
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "review"
