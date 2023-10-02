27ca427999a928389b15711c4f1ca1d00349fe51
yeonjun <yeonjunky@gmail.com>
2023-10-02 05:19:06 +0000

    ✨allow methods put, delete through '/review'

--- review/urls.py
+++ review/urls.py
@@ -5,16 +5,9 @@ from review.views import ReviewViewSet
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'review', ReviewViewSet)
 
-router.routes[0] = routers.Route(
-        url=r'^{prefix}{trailing_slash}$',
-        mapping={
-            'get': 'list',
-            'put': 'create'
-        },
-        name='{basename}-list',
-        detail=False,
-        initkwargs={'suffix': 'List'}
-    )
+router_mapping = router.routes[0].mapping
+router_mapping['put'] = 'upsert'
+router_mapping['delete'] = 'destroy'
 
 urlpatterns = []
 # [path('some_functional_view_rul', views.functional_view)]
