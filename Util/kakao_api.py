9a0f4f2009332024d63b2027ca83303c4fb33763
woohyun <woohyun212@gmail.com>
2023-10-02 13:23:17 +0000

    🧑‍💻 Add get_search_result_by_address method

--- Util/kakao_api.py
+++ Util/kakao_api.py
@@ -1,5 +1,5 @@
 import requests, json
-
+from pprint import pprint 
 
 class KakaoAPI:
     def __init__(self, kakao_rest_api_key:str):
@@ -17,15 +17,51 @@ class KakaoAPI:
         ex: "제주도 서귀포시 상예로 319"
         ('126.39471590891', '33.2731855026733')
         """
-        headers = self.auth_headers.copy()
-        params = {"query": address}
-        headers.update(headers)
-        params.update(params)
-        req = requests.get(self.HOST+self.SEARCH_KEYWORD_URL,
+        headers.update(self.auth_headers.copy())
+        params.update({"query": address})
+        
+        req = requests.get(self.HOST+self.SEARCH_ADDRESS_URL,
                            headers=headers, params=params)
         json_data = json.loads(req.text)
         documents = json_data['documents'][0]
         return documents['x'], documents['y']
+    
+    
+    def get_search_result_by_address(self, address:str, headers:dict={}, params:dict={}):
+        """
+        https://developers.kakao.com/docs/latest/ko/local/dev-guide#address-coord
+
+        ex: "제주도 서귀포시 상예로 319"
+        ('126.39471590891', '33.2731855026733')
+        """
+        headers.update(self.auth_headers.copy())
+        page = 0
+        params.update({"query": address, "page": page})
+        try:
+            while True:
+                page += 1
+                params.update({"page": page})
+                json_data = json.loads(
+                    requests.get(self.HOST+self.SEARCH_ADDRESS_URL,
+                           headers=headers, params=params).text
+                            )
+                documents = json_data["documents"]
+                for doc in documents:
+                    # 내가 찾는 문서면 리턴
+                    if doc["address_name"] == address:
+                        return doc
+                # 못찾았으면 종료
+                if json_data["meta"]["is_end"] == True:
+                    break
+            # 못찾았으면 마지막에 검색된 데이터를 documents에 넣고
+            # 비어있으면 None 있으면 검색된 거라도 줌
+            documents = json_data["documents"]
+            if documents:
+                return documents[0]
+            return None
+        
+        except KeyError:
+            return {"json_data": json_data, "address":address, "page": page}
 
     def get_search_result_by_keyword(self, keyword:str, headers:dict={}, params:dict={}):
         """
