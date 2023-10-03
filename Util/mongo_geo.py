ebeccd726d71cf44ea643f15a8e6282799473255
woohyun <woohyun212@gmail.com>
2023-10-03 15:56:12 +0000

    🐛Fix 'list index out of range' -검색 결과가 없으면 list index out of range 가 나오게 되어 검색 데이터가 없을 경우 빈 배열을 반환하도록 처리했습니다

--- Util/mongo_geo.py
+++ Util/mongo_geo.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     import os, sys
     sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-    from pprint import pprint
+from pprint import pprint
 
 from MiliDeal.settings import env_keys 
 from pymongo import MongoClient
@@ -32,8 +32,12 @@ def get_slugs_with_geoWithin_search(coordinates:list, distance:int):
         "$geoWithin":{"$centerSphere": [coordinates, distance / 6378160]}}}})
     pipeline.append({"$group": {"_id" : "slugs","slugs": { "$push": "$slug" }}})
     pipeline.append({"$project": { "_id":0} })
-    return list(mongo_client[env_keys('mongo_database')].stores.aggregate(pipeline))[0]["slugs"]
+    # 검색 결과가 없으면 list index out of range 가 나옴
+    results = list(mongo_client[env_keys('mongo_database')].stores.aggregate(pipeline))
+    if results:
+        return results[0]["slugs"]
+    return []
 
 if __name__ == "__main__":
-    searched_slugs = get_slugs([127.7816977, 37.9296045], 120)
+    searched_slugs = get_slugs_with_geoWithin_search([127.02927482937886, 37.5243205724172], 120)
     pprint(searched_slugs)
