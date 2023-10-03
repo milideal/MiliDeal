624a74363edbd606b82a8b321f17d1925ac950e9
woohyun <woohyun212@gmail.com>
2023-10-03 12:06:01 +0000

    ✨Show review Info at `/store` - Store API 요청시 리뷰 정보도 포함되어 나옵니다. 💩- pagination이 여기에서는 적용아마 안될 것 같은데 추후에 수정하면 될 것 같습니다. - List 와 Detail 에서 보여주는 정보의 차이를 두기위해 Detail을 상속하는 List를 만들었습니다,

--- store/serializers.py
+++ store/serializers.py
@@ -1,30 +1,64 @@
-from rest_framework import serializers
-from .models import StoreModel
-from pprint import pprint
 from collections import OrderedDict
+from django.db.models import Avg
+from rest_framework import serializers
 import json
 
-class StoreSerializer(serializers.HyperlinkedModelSerializer):
+from store.models import StoreModel
+from review.serializers import ReviewSerializer
+from review.models import Review as ReviewModel
+
+from pprint import pprint
+
+
+
+class StoreDetailSerializer(serializers.HyperlinkedModelSerializer):
+    score__avg = serializers.SerializerMethodField(read_only=True)
+    reviews = serializers.SerializerMethodField(read_only=True)
+    
+    def get_score__avg(self, obj):
+        obj.reviews.aggregate(Avg('score'))
+        return obj.reviews.aggregate(Avg('score'))["score__avg"]
+    
+    def get_reviews(self, obj):
+        # 역참조는 피참조 모델 인스턴스에서 정참조 필드의 related_name 속성으로 접근할 수 있음
+        reviews = ReviewSerializer(obj.reviews, read_only=True, many=True).data
+        return reviews
+    
     def to_representation(self, instance):
         res = super().to_representation(instance)
         res["coord"] = {
             "x": instance.coord["coordinates"][0],  # 경도
             "y": instance.coord["coordinates"][1]   # 위도
-            }  
+        }
         return res
 
     def to_internal_value(self, data):
         _data = data.copy()
-        coord = json.loads(data["coord"].replace("'", '"'))
+        # coord = json.loads(data["coord"].replace("'", '"'))
         _data["coord"] = {
             "type": "Point",
-            "coordinates": [coord["x"], coord["y"]]
-            }
+            "coordinates": [_data["coord"]["x"], _data["coord"]["y"]]
+        }
         return super().to_internal_value(_data)
-        
+
+    
     class Meta:
         model = StoreModel
-        fields = "__all__"
+        fields = "__all__"    
+        # fields = ["url","slug", "address", "coord", "name", "storeType", "imageSrc",
+            # "target", "promotion", "tel", "facilities", "homepage", "endDate", "reviews",  "score__avg",]
         extra_kwargs = {
             'url': {'lookup_field': 'slug'}
         }
+
+class StoreListSerializer(StoreDetailSerializer):
+    class Meta:
+        model = StoreModel     
+        # fields = "__all__"
+        fields = ["url","slug", "address", "coord", "name", "storeType", 
+            "target", "promotion", "tel",  "homepage",  "score__avg",]
+        extra_kwargs = {
+            'url': {'lookup_field': 'slug'}
+        }
+
+    pass
--- store/views.py
+++ store/views.py
@@ -1,22 +1,29 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.response import Response
 from django.db.models.query import QuerySet
-from .serializers import StoreSerializer
-from .models import StoreModel
-import djongo.models
+
 from Util.mongo_geo import get_slugs_with_geoWithin_search
-from pprint import pprint
+from store.serializers import StoreListSerializer, StoreDetailSerializer
+from store.models import StoreModel
+
+from pprint import pprint, pp
 
 
 class StoreViewSets(ModelViewSet):
     queryset = StoreModel.objects.all()
-    serializer_class = StoreSerializer
+    serializer_class = StoreListSerializer
     lookup_field = 'slug'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = StoreDetailSerializer(instance, context={'request': request})
+        return Response(serializer.data)
 
 class StoreGeoSearchViewSets(ReadOnlyModelViewSet):
     # queryset = StoreModel.objects.all()
-    serializer_class = StoreSerializer
+    serializer_class = StoreListSerializer
     lookup_field = "slug"
-
+    
     def get_queryset(self, x:float=None, y:float=None):
         coord_x = self.request.query_params.get('x')
         coord_y = self.request.query_params.get('y')
@@ -32,42 +39,10 @@ class StoreGeoSearchViewSets(ReadOnlyModelViewSet):
             "'%s' should either include a `queryset` attribute, "
             "or override the `get_queryset()` method."
            % self.__class__.__name__
-        )      
-        
-        """
-        Util.mongo_geo.py 로 데이터를 불러오면 결국 Dict(원래는 Cursor)객체라
-        # searched_instance = [StoreModel(**data) for data in searched]
-        StoreModel에 넣어봤는데 list에 모은다고 queryset이 되는게 아님..
-        list에 넣고 QuerySet으로 형변환이 될줄 알았는데 안됨.
-
-        Model instance로 바꿔줄려면 검색된 애들로 Filter를 다시해야됨. > 연산이 3배
-        DB에서라도 연산의 최소화를 해야함..
-        
-        find 에서 aggregate 로 slug만 가져오게 함.
-        처음부터 DB에서 slug 만 불러오면 좀 낫지 않을까 싶었음.
-        모델에서 slug 만 불러오고 그걸로 filter 걺.
-        그럼 정상적으로 QuerySet 객체로 반환됨.
-        """
+        )
 
         queryset = self.queryset
         if isinstance(queryset, QuerySet):
             # Ensure queryset is re-evaluated on each request.
             queryset = queryset.all()
         return queryset
-
-    # reference를 읽어보니 queryset 만 수정하면 될 것 같음!
-    # def list(self, request, *args, **kwargs):
-    #     assert 'x' in request.GET and 'y' in request.GET, (
-    #         f" X, Y 주세요")
-    #     coord_x = request.GET['x']
-    #     coord_y = request.GET['y']
-    #     # 쿼리셋을 불러와서 필터 쿼리셋에 쳐박고
-    #     queryset = self.filter_queryset(self.get_queryset(coord_x, coord_y))
-    #     # 페이지네이션해서 내보냄
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-    #     반환하기 전에는 직렬화
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
