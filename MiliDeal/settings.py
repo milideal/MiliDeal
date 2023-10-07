eda8e26eb935d228430f4682d6142e1e2a8353c7
woohyun <woohyun212@gmail.com>
2023-10-07 06:53:17 +0000

    🔧Increase PAGE_SIZE - ℹ️ Increased PAGE_SIZE due to limited data, adjusting pagination

--- MiliDeal/settings.py
+++ MiliDeal/settings.py
@@ -219,7 +219,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 30
 }
 
 # JWT (JSON Web Token) 설정.
