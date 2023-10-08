4b942cb49de4df6f99b6859be07ab0f4616315d3
woohyun <woohyun212@gmail.com>
2023-10-08 13:35:26 +0000

    ✨Add 'version_info' FBV

--- mainAPI/views.py
+++ mainAPI/views.py
@@ -2,7 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import TestSerializer
 from .models import TestModel
 from Util.decorators import convert_objectId
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
+def version_info(request):
+    if request.method == 'GET':
+        return JsonResponse({"data_amount":"886",
+                             "last_update":"2023-10-07"})
 
 class TestViewSets(ModelViewSet):
     queryset = TestModel.objects.all()
