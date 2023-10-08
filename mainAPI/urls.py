447c23693176f7a9cef22b0f89669bd1faf46474
woohyun <woohyun212@gmail.com>
2023-10-08 13:33:17 +0000

    ✨Add 'version_info' endpoint

--- mainAPI/urls.py
+++ mainAPI/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import TestViewSets
+from .views import TestViewSets, version_info
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'test', TestViewSets)
-
 urlpatterns = [
-    path('', include(router.urls))
+    path('version_info', version_info),
 ]
 # [path('some_functional_view_rul', views.functional_view)]
 
